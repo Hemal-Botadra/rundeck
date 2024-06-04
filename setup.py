1bb4d14ea239bd84811dabb620fe6f2602f903fa
Hemal-Botadra <botadrahemal@gmail.com>
2024-06-04 14:01:49 +0530

    check

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+install_requires = [
+    "ddtrace",
+    "grpcio",
+    "protobuf",
+    "pxproto",
+    "python-json-logger",
+    "redis[hiredis]<4.4",
+    "uvloop",
+    "Pillow",
+    "apache-airflow[kubernetes]==2.9.0",
+]
+
+setup(
+    name='my-package',
+    version=f'1.2.3',
+    description='This is an awesome Python package',
+    author='Hemal',
+    install_requires=install_requires,
+    license='MIT',
+)
